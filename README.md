5a245122c4c8edb3501a0bfab2eedcb859cc9ecb
mgeier63 <mgeier63@gmail.com>
2015-08-27 06:29:47 +0200

    stuff

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Demo App to demonstrate data truncation in current OpenPGPApi lib
+
+
+### Build with Gradle
+
+1. Clone the project from GitHub
+2. Get all external submodules with ``git submodule update --init --recursive``
+3. Execute ``./gradlew build``
+4. This builds TWO apks, one ("orig") with the original, unpatched OpenPGPApi
+5. and one ("fixed") using my fixed OpenPGPApi
+
+### Demonstrate the bug
+
+1. Make sure OpenKeyChain is installed
+2. Now install the _orig_ app with ``adb install -r OpenPgpApiLibDemobuild/app/build/outputs/apk/app-orig-debug.apk``
+3. run the app, selecte a key and then tap "Test..."
+4. you should see "Data Truncated on N of 100 invocations"
+
+### Verify the fix
+1. Build openkeychain after having merged this pull request: XXXXXXXXXXXXXXXXX
+2. Nowinstall the _fixed_ app with ``adb install -r OpenPgpApiLibDemobuild/app/build/outputs/apk/app-fixed-debug.apk``
+3. run the app, selecte a key and then tap "Test..."
+4. no data truncation any more, relax!
---
+++ app/src/fixed/java/mgeier63/openpgpapilibdemo/Api.java
@@ -0,0 +1,51 @@
+package mgeier63.openpgpapilibdemo;
+
+import android.content.Context;
+import android.content.Intent;
+
+
+import org.openintents.openpgp.IOpenPgpService2;
+import org.openintents.openpgp.util.OpenPgpApi;
+import org.openintents.openpgp.util.OpenPgpServiceConnection;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class Api implements OpenPgpServiceConnection.OnBound, MainActivity.IApi {
+    private final Context mCtx;
+    OpenPgpApi mApi;
+
+    public Api(Context ctx) {
+        mCtx = ctx;
+        OpenPgpServiceConnection sc = new OpenPgpServiceConnection(ctx, "org.sufficientlysecure.keychain", this);
+        sc.bindToService();
+    }
+
+    public Intent execute(Intent data, InputStream is, OutputStream os) {
+        return mApi.executeApi(data, is, os);
+    }
+
+
+    @Override
+    public Intent executeApi(Intent data, InputStream is, OutputStream os) {
+        return mApi.executeApi(data, is, os);
+    }
+
+    @Override
+    public boolean isServiceBound() {
+        return mApi != null;
+    }
+
+    @Override
+    public void onBound(IOpenPgpService2 service) {
+        mApi = new OpenPgpApi(mCtx, service);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        e.printStackTrace();
+        OpenPgpServiceConnection sc = new OpenPgpServiceConnection(mCtx, "org.sufficientlysecure.keychain.debug", this);
+        sc.bindToService();
+    }
+}
